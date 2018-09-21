494dbade58555f6584765e120915bc4e82da1ff0
larryjung <corbswjd@gmail.com>
2018-09-21 15:12:02 +0900

    Add Rook info test

--- src/test/java/pieces/unitInfo/QueenInfoTest.java
+++ src/test/java/pieces/unitInfo/QueenInfoTest.java
@@ -1,4 +1,21 @@
 package pieces.unitInfo;
 
+import org.junit.Test;
+import pieces.Piece;
+import pieces.Player;
+import pieces.coordinate.Coordinate;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 public class QueenInfoTest {
+
+    @Test(expected = RuntimeException.class)
+    public void directionConditionCheck() {
+        QueenInfo queenInfo = new QueenInfo(Piece.QUEEN, Player.BLACK, new Coordinate(1, 'a'));
+        queenInfo.directionConditionCheck(new Coordinate(2, 'c'));
+    }
+
+
+
 }
--- src/test/java/pieces/unitInfo/RookInfoTest.java
+++ src/test/java/pieces/unitInfo/RookInfoTest.java
@@ -1,4 +1,30 @@
 package pieces.unitInfo;
 
+import org.junit.Test;
+import pieces.Piece;
+import pieces.Player;
+import pieces.coordinate.Coordinate;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 public class RookInfoTest {
+
+    @Test(expected = RuntimeException.class)
+    public void directionConditionCheck_fail() {
+        RookInfo rookInfo = new RookInfo(Piece.ROOK, Player.BLACK, new Coordinate(1, 'a'));
+        rookInfo.directionConditionCheck(new Coordinate(2, 'b'));
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void directionConditionCheck_fail_2() {
+        RookInfo rookInfo = new RookInfo(Piece.ROOK, Player.BLACK, new Coordinate(1, 'a'));
+        rookInfo.directionConditionCheck(new Coordinate(2, 'c'));
+    }
+
+    @Test
+    public void directionConditionCheck_1() {
+        RookInfo rookInfo = new RookInfo(Piece.ROOK, Player.BLACK, new Coordinate(1, 'a'));
+        assertThat(rookInfo.directionConditionCheck(new Coordinate(1, 'f')), is(rookInfo));
+    }
 }
